26d931ed8413c033bf329f41029dfe74f6292e16
StudentPP1 <mishamakytonin@gmail.com>
2026-03-29 21:20:08 +0300

    fix: change naming & add mywebapp.socket

--- mywebapp/scripts/03_build_and_systemd.sh
+++ mywebapp/scripts/03_build_and_systemd.sh
@@ -9,11 +9,24 @@ echo "6. Copy jar file"
 mkdir -p /opt/mywebapp
 cp build/libs/*-SNAPSHOT.jar /opt/mywebapp/mywebapp.jar
 # Owner of folder is system user: mywebapp
-chown -R mywebapp:mywebapp /opt/mywebapp
+chown -R app:app /opt/mywebapp
 # Only read & execute
 chmod 500 /opt/mywebapp/mywebapp.jar
 
 echo "7. Configuring Systemd"
+
+# /etc/systemd/system/mywebapp.socket
+cat <<EOF > /etc/systemd/system/mywebapp.socket
+[Unit]
+Description=MyWebApp Socket
+
+[Socket]
+ListenStream=5000
+Accept=no
+
+[Install]
+WantedBy=sockets.target
+EOF
 
 # Create the service file (config to auto run)
 # After=network.target mariadb.service: run after we have network & db
@@ -23,10 +36,11 @@ cat <<EOF > /etc/systemd/system/mywebapp.service
 [Unit]
 Description=MyWebApp Service
 After=network.target mariadb.service
+Requires=mywebapp.socket
 
 [Service]
-User=mywebapp
-Group=mywebapp
+User=app
+Group=app
 WorkingDirectory=/opt/mywebapp
 ExecStart=/usr/bin/java -jar /opt/mywebapp/mywebapp.jar --spring.datasource.url=jdbc:mariadb://localhost:3306/simple_inventory --spring.datasource.username=mywebapp --spring.datasource.password=root --server.port=5000
 SuccessExitStatus=143
@@ -35,11 +49,7 @@ SuccessExitStatus=143
 WantedBy=multi-user.target
 EOF
 
-# kill & delete old socket
-systemctl stop mywebapp.socket 2>/dev/null || true
-systemctl disable mywebapp.socket 2>/dev/null || true
-rm -f /etc/systemd/system/mywebapp.socket
-
 systemctl daemon-reload # reload daemon to clean start service
+# systemctl enable --now mywebapp.socket -> prevent port conflict with spring app
 systemctl enable --now mywebapp.service # run service
 systemctl restart mywebapp.service # restart to pull new app config
